b851c466e26275eb5aec87b715e9b4b7faa62494
Oliver <oliver.wolfertz@haw-hamburg.de>
2023-12-01 20:10:21 +0100

    PP2 Setup

---
+++ PP2.py
@@ -0,0 +1,13 @@
+#Description: locate the direction of the sound source
+
+import numpy as np
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+
+fs, y = read("PP2Data/D1azi_45,0_ele_0,0.wav")
+#location of the sound source in degree
+
+
+
+
+
