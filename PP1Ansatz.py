abcea468ec347e205cca66c11d03b65682007ed8
Oliver <oliver.wolfertz@haw-hamburg.de>
2023-12-01 10:58:55 +0100

    Niklas Ansatz + T60 + C50 + C80

--- PP1Ansatz.py
+++ PP1Ansatz.py
@@ -16,7 +16,27 @@ for i in range(0, len(y)):
     E_log = 10*np.log10(E/E_ges)
     E_array.append(E_log)
 
-print(E_ges)
+#search the time where the energy is -10dB
+t10 = None
+for i in range(0, len(E_array)):
+    if E_array[i] <= -10:
+        if t10 is None:
+            t10 = i/fs
+
+t20 = None
+for i in range(0, len(E_array)):
+    if E_array[i] <= -20:
+        if t20 is None:
+            t20 = i/fs
+
+T60 = (60*(t20-t10))/10
+print(f" t10: {t10}\n t20: {t20}\n T60: {T60}")
+
+#Sprachverständlichkeit C50 und C80
+C50 = np.sum(np.square(y_normiert[0:round(fs/2)]))/fs
+C80 = np.sum(np.square(y_normiert[0:round(fs*0.8)]))/fs
+print(f" C50: {C50}\n C80: {C80}\n Gesamtenergie: {E_ges}")
+
 
 fig, axes = plt.subplots()
 axes.plot(timescale, E_array, label="LE(t)")
