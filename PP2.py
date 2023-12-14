e31e6356485722d8cee7b3cb655d8a73c0b47ccc
Oliver <oliver.wolfertz@haw-hamburg.de>
2023-12-14 19:30:35 +0100

    Lineares Panning + Konstantes Panning + -4.5dB Panning

--- PP2.py
+++ PP2.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.io.wavfile import read
 import matplotlib.pyplot as plt
 import sounddevice as sd
-import PP2Gui as gui
+#import PP2Gui as gui
 
 
 
@@ -20,24 +20,37 @@ else:
     LR = y
     print(f"**Stereo**")
 LRBackup = LR
-#Lineares Panning
 
+
+#Lineares Panning
 def linear_pan(LR, grad):
     RAD = grad*(np.pi/180) #Umrechnung von Grad in Radiant
     for i in range(len(LR[:, 0])):
         LR[:, 0][i] *= (2 / np.pi) * (np.pi / 2 - RAD)
         LR[:, 1][i] *= (2 / np.pi) * RAD
 
+#Panning mit Konstanter Leistung
+def konstant_pan(LR, grad):
+    RAD = grad*(np.pi/180) #Umrechnung von Grad in Radiant
+    for i in range(len(LR[:, 0])):
+        LR[:, 0][i] *= np.cos(RAD)
+        LR[:, 1][i] *= np.sin(RAD)
 
-
-
-
+#-4.5dB Panning: Mix aus Konstant und Linear
+def Mix_pan(LR, grad):
+    RAD = grad*(np.pi/180) #Umrechnung von Grad in Radiant
+    for i in range(len(LR[:, 0])):
+        LR[:, 0][i] *= np.sqrt((2/np.pi) * (np.pi/2 - RAD) * np.cos(RAD))
+        LR[:, 1][i] *= np.sqrt((2/np.pi) * RAD * np.sin(RAD))
 
 def play():
     sd.play(LR, fs)
     sd.wait()
 
-linear_pan(LRBackup, 120)
+
+linear_pan(LR, 45)
+#konstant_pan(LR, 45)
+#mix_pan(LR, 45)
 play()
 
 
