2795fdbf8b96a2ecdd43ef1878f56085cc10e0c1
´revilo95 <revilo95@gmail.com>
2023-12-29 13:06:41 +0100

    Rechenzeit effektiver gestaltet

--- PP3.py
+++ PP3.py
@@ -5,35 +5,40 @@ import matplotlib.pyplot as plt
 import sounddevice as sd
 
 #make a sinus
-
 freq = 440
 fs = 48000
 duration = 1
 n = 15
 
+
+#Erzeugung eines exponentiell abklingenden Sinus
 rateArray = np.arange(0, duration, 1 / fs)
 fade = np.logspace(0, -3, int(fs * duration), endpoint=False)
 sine = fade * np.sin(2 * np.pi * freq * rateArray)
 
 
 delayArray = np.zeros(fs*duration*n)
-dings = int((fs/1000)*500)
-delayArray[0:len(sine)] = sine
+
+delayInMs = 200
+timeInSamples = int((fs/1000)*delayInMs)
+
 #reversed sine:
-revsine = sine[::-1]
+revAudio = sine[::-1]
+plt.plot(revAudio)
+plt.plot(sine)
+plt.show()
+
 
 for i in range(0, n):
-    delayArray[dings*i:len(revsine)+dings*i] = revsine
-    revsine = revsine * 0.8
+    delayArray[timeInSamples*i:len(revAudio)+timeInSamples*i] = revAudio
+    revAudio = revAudio * 0.8
+#delayArray[0:len(sine)] = sine
 
 
-
-
-
-"""
 for i in range(0, n):
     delayArray[(len(sine))*i:len(sine)*(i+1)] = sine/i
-"""
+
+
 #################################################################
 sd.play(delayArray, fs)
 sd.wait()
